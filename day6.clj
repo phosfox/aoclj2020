7fe30a4f0f9c59964fe1b509d35b16d3fa6b4830
phosfox <mordredur@googlemail.com>
2020-12-06 16:15:15 +0100

    solves day6

---
+++ day6.clj
@@ -0,0 +1,43 @@
+(ns aoclj2020
+  (:require [clojure.string :as str]
+            [clojure.set :as set]))
+
+(def test-input
+  "abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b")
+
+(def real-input (slurp "day6.input"))
+
+(defn solve1 [inp]
+  (->> (str/split inp #"\R\R")
+     (map #(str/replace % #"\n" ""))
+     (map set)
+     (map count)
+     (reduce +)))
+
+(defn solve2 [inp]
+  (->> (str/split inp #"\R\R")
+       (map str/split-lines)
+       (map #(map set %))
+       (map #(apply set/intersection %))
+       (map count)
+       (reduce +)))
+
+(solve1 real-input)
+(solve2 real-input)
+
+
+(set/intersection #{"a" "b"} #{"c" "b"})
